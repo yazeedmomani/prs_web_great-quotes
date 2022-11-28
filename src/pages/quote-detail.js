eb0f137f74d6b9caf6473a6be0650fd5e5ccf1b1
Yazeed al-Momani <yazeed.02.momani@outlook.com>
2022-11-28 10:21:42 +0300

    add comments page

--- src/pages/quote-detail.js
+++ src/pages/quote-detail.js
@@ -1,9 +1,18 @@
-import { useParams } from "react-router-dom";
+import { useParams, Route } from "react-router-dom";
+import { Fragment } from "react";
+import Comments from "../components/comments/Comments";
 
 function QuoteDetail() {
   const param = useParams();
 
-  return <h1>{param.quoteId}</h1>;
+  return (
+    <Fragment>
+      <h1>{param.quoteId}</h1>;
+      <Route path={`/quotes/${param.quoteId}/comments`}>
+        <Comments />
+      </Route>
+    </Fragment>
+  );
 }
 
 export default QuoteDetail;
