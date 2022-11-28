f89f2e446e574b6e476b8347dee1f4fe534a3059
Yazeed al-Momani <yazeed.02.momani@outlook.com>
2022-11-28 12:49:02 +0300

    implement url search

--- src/components/quotes/QuoteList.js
+++ src/components/quotes/QuoteList.js
@@ -1,11 +1,28 @@
-import { Fragment } from 'react';
+import { Fragment } from "react";
 
-import QuoteItem from './QuoteItem';
-import classes from './QuoteList.module.css';
+import QuoteItem from "./QuoteItem";
+import classes from "./QuoteList.module.css";
+import { useHistory, useLocation } from "react-router-dom";
 
 const QuoteList = (props) => {
+  const history = useHistory();
+  const location = useLocation();
+
+  const urlParams = new URLSearchParams(location.search);
+  const isAscending = urlParams.get("sort") === "asc";
+
+  const sortHandler = function (e) {
+    e.preventDefault();
+    history.push(`/quotes?sort=${isAscending ? "desc" : "asc"}`);
+  };
+
   return (
     <Fragment>
+      <div className={classes.sorting}>
+        <button onClick={sortHandler}>{`Sort ${
+          isAscending ? "Descending" : "Ascending"
+        }`}</button>
+      </div>
       <ul className={classes.list}>
         {props.quotes.map((quote) => (
           <QuoteItem
